ab954f46ac4e568271f95bb8a133d54c260c05dc
paolahern <paolahernanreyes@gmail.com>
2026-03-13 00:49:18 -0600

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,45 +1,50 @@
 // Nombre de la caché
-const CACHE = "mosaico-cache-v4";
-
-// Archivos que se guardarán para que la app funcione instalada u offline
+const CACHE = "mosaico-cache-v5";
+//CARGA LOS ARCHIVOS 
 const archivos = [
-"./",
-"./index.html",
-"./main.js",
-"./carga.js",
-"./inicio.js",
-"./juego.js",
+"/",
+"/index.html",
+"/main.js",
+"/carga.js",
+"/inicio.js",
+"/juego.js",
 
-// IMÁGENES
-"./1.jpg",
-"./2.jpeg",
-"./3.jpg",
-"./fondo.jpg",
-"./icono.png"
+"/1.jpg",
+"/2.jpeg",
+"/3.jpg",
+"/fondo.jpg",
+"/icono.png"
 ];
 
-// Instalación del Service Worker
 self.addEventListener("install", (event) => {
+
 event.waitUntil(
-caches.open(CACHE).then(cache => cache.addAll(archivos))
+caches.open(CACHE)
+.then(cache => cache.addAll(archivos))
 );
+
+self.skipWaiting();
+
+});
+
+self.addEventListener("activate", (event) => {
+
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys.filter(key => key !== CACHE)
+.map(key => caches.delete(key))
+);
+})
+);
+
 });
 
 self.addEventListener("fetch", (event) => {
 
 event.respondWith(
-fetch(event.request)
-.then(response => {
-
-const copia = response.clone();
-
-caches.open(CACHE).then(cache => {
-cache.put(event.request, copia);
-});
-
-return response;
-
-})
-.catch(() => caches.match(event.request))
+caches.match(event.request)
+.then(res => res || fetch(event.request))
 );
+
 });
