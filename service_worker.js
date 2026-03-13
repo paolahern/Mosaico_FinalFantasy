e792df35ac59541895719a9c1dbdfba862f130e7
paolahern <paolahernanreyes@gmail.com>
2026-03-13 00:19:55 -0600

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,31 +1,39 @@
-const CACHE = "mosaico-cache-v1"
+// Nombre de la caché
+const CACHE = "mosaico-cache-v2";
 
+// Archivos que se guardarán para que la app funcione instalada u offline
 const archivos = [
-"/index.html",
-  "/main.js",
-  "/carga.js",
-  "/inicio.js",
-  "/juego.js",
+"./",
+"./index.html",
+"./main.js",
+"./carga.js",
+"./inicio.js",
+"./juego.js",
 
-  // IMÁGENES
-  "/1.jpg",
-  "/2.jpeg",
-  "/3.jpg",
-  "/fondo.jpg",
-  "/icono.png"
+// IMÁGENES
+"./1.jpg",
+"./2.jpeg",
+"./3.jpg",
+"./fondo.jpg",
+"./icono.png"
 ];
 
+// Instalación del Service Worker
 self.addEventListener("install", (event) => {
 
 event.waitUntil(
-caches.open(CACHE).then(cache => cache.addAll(archivos))
-)
+caches.open(CACHE)
+.then(cache => cache.addAll(archivos))
+);
+
 });
 
+// Intercepta las peticiones y busca primero en caché
 self.addEventListener("fetch", (event) => {
 
 event.respondWith(
-caches.match(event.request).then(res => res || fetch(event.request))
-)
+caches.match(event.request)
+.then(res => res || fetch(event.request))
+);
 
 });
