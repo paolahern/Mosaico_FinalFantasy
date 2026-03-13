640b1fc981b6dd4b155e45b23f01618ddaa36794
paolahern <paolahernanreyes@gmail.com>
2026-03-13 00:50:21 -0600

    Update carga.js

--- carga.js
+++ carga.js
@@ -42,10 +42,10 @@ class Carga extends Phaser.Scene {
         ).setOrigin(0, 0.5);
 
         // archivos a cargar
-        this.load.image("fondo", "fondo.jpg");
-        this.load.image("1", "1.jpg");
-        this.load.image("2", "2.jpeg");
-        this.load.image("3", "3.jpg");
+        this.load.image("fondo", "./fondo.jpg");
+        this.load.image("1", "./1.jpg");
+        this.load.image("2", "./2.jpeg");
+        this.load.image("3", "./3.jpg");
 
         // progreso de carga
         this.load.on("progress", (valor) => {
@@ -73,3 +73,4 @@ class Carga extends Phaser.Scene {
 
 
 }
+
